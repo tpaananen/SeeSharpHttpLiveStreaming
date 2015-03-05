bf00fc314fa50f8bf39a55af8a73af45cba92504
tpaananen <tommi.paananen@outlook.com>
2015-03-05 20:16:51 +0200

    Using TagFactory

--- SeeSharpHttpLiveStreaming/Playlist/Tags/BaseTag.cs
+++ SeeSharpHttpLiveStreaming/Playlist/Tags/BaseTag.cs
@@ -44,95 +44,9 @@ namespace SeeSharpHttpLiveStreaming.Playlist.Tags
                 throw new InvalidOperationException("The start tag cannot be created.");
             }
 
-            // TODO: replace switch case with some cacheable factory dictionary
-
-            BaseTag tagObject;
-            if (Tag.IsMediaSegmentTag(line.Tag))
-            {
-                tagObject = CreateMediaSegment(line);
-            }
-            else if (Tag.IsMasterTag(line.Tag))
-            {
-                tagObject = CreateMasterTag(line);
-            }
-            else if (Tag.IsMediaPlaylistTag(line.Tag))
-            {
-                tagObject = CreateMediaPlaylistTag(line);
-            }
-            else if (Tag.IsBasicTag(line.Tag))
-            {
-                tagObject = CreateBasicTag(line);
-            }
-            else
-            {
-                throw new SerializationException("No handling for tag " + line.Tag);
-            }
-            
+            var tagObject = TagFactory.Create(line.Tag);
             tagObject.Deserialize(line.GetParameters(), version);
             return tagObject;
-        }
-
-        private static BaseTag CreateMasterTag(PlaylistLine line)
-        {
-            switch (line.Tag)
-            {
-                case "#EXT-X-STREAM-INF":
-                    return new StreamInf();
-
-                case "#EXT-X-MEDIA":
-                    return new ExtMedia();
-
-                default:
-                    throw new SerializationException("No handling for tag " + line.Tag);
-            }
-        }
-
-        private static BaseTag CreateMediaPlaylistTag(PlaylistLine line)
-        {
-            switch (line.Tag)
-            {
-                case "#EXT-X-TARGETDURATION":
-                    return new TargetDuration();
-
-                default:
-                    throw new SerializationException("No handling for tag " + line.Tag);
-            }
-        }
-
-        private static BaseTag CreateBasicTag(PlaylistLine line)
-        {
-            if (line.Tag != "#EXT-X-VERSION")
-            {
-                throw new NotSupportedException("The basic tag " + line.Tag + " is not supported at this point.");
-            }
-            return new ExtXVersion();
-        }
-
-        private static BaseTag CreateMediaSegment(PlaylistLine line)
-        {
-            switch (line.Tag)
-            {
-                case "#EXTINF":
-                    return new ExtInf();
-                    
-                case "#EXT-X-BYTERANGE":
-                    return new ExtXByteRange();
-
-                case "#EXT-X-DISCONTINUITY":
-                    throw new NotImplementedException("EXT-X-DISCONTINUITY");
-
-                case "#EXT-X-KEY":
-                    throw new NotImplementedException("EXT-X-KEY");
-                    
-                case "#EXT-X-MAP":
-                    throw new NotImplementedException("EXT-X-MAP");
-                    
-                case "#EXT-X-PROGRAM-DATE-TIME":
-                    throw new NotImplementedException("EXT-X-PROGRAM-DATE-TIME");
-
-                default:
-                    throw new NotSupportedException("Tag " + line.Tag + " is not supported as Media Segment tag.");
-            }
         }
     }
 }
