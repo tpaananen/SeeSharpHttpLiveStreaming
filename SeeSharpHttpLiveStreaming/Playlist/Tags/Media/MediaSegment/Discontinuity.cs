7cf0057f8443e14f10ef77403c6d6c6238918886
tpaananen <tommi.paananen@outlook.com>
2015-03-06 18:42:18 +0200

    Added discontinuity tag

---
+++ SeeSharpHttpLiveStreaming/Playlist/Tags/Media/MediaSegment/Discontinuity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using SeeSharpHttpLiveStreaming.Utils;
+
+namespace SeeSharpHttpLiveStreaming.Playlist.Tags.Media.MediaSegment
+{
+    /// <summary>
+    /// Represents the EXT-X-DISCONTINUITY tag.
+    /// </summary>
+    /// <remarks>
+    /// The EXT-X-DISCONTINUITY tag indicates a discontinuity between the
+    /// Media Segment that follows it and the one that preceded it.
+    ///
+    /// Its format is:
+    ///
+    /// #EXT-X-DISCONTINUITY
+    ///
+    /// The EXT-X-DISCONTINUITY tag MUST be present if there is a change in
+    /// any of the following characteristics:
+    ///
+    /// o  file format
+    /// o  number, type and identifiers of tracks
+    /// o  timestamp sequence
+    ///
+    /// The EXT-X-DISCONTINUITY tag SHOULD be present if there is a change in
+    /// any of the following characteristics:
+    /// 
+    /// o  encoding parameters
+    /// o  encoding sequence
+    /// 
+    /// See Section 3, Section 6.2.1, and Section 6.3.3 for more information
+    /// about the EXT-X-DISCONTINUITY tag.
+    /// </remarks>
+    public class Discontinuity : BaseTag
+    {
+        /// <summary>
+        /// Gets the name of the tag, for example EXT-X-MEDIA.
+        /// </summary>
+        public override string TagName
+        {
+            get { return "#EXT-X-DISCONTINUITY"; }
+        }
+
+        /// <summary>
+        /// Gets the type of the tag.
+        /// </summary>
+        public override TagType TagType
+        {
+            get { return TagType.ExtXDiscontinuity; }
+        }
+
+        /// <summary>
+        /// Deserializes an object.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="version">The version.</param>
+        public override void Deserialize(string content, int version)
+        {
+            if (!string.IsNullOrEmpty(content))
+            {
+                throw new SerializationException("The EXT-X-DISCONTINUITY tag must not have any attributes.");
+            }
+        }
+    }
+}
---
+++ SeeSharpLiveStreaming.Tests/Playlist/Tags/TestExtXDiscontinuity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.Serialization;
+using NUnit.Framework;
+using SeeSharpHttpLiveStreaming.Playlist;
+using SeeSharpHttpLiveStreaming.Playlist.Tags.Media.MediaSegment;
+
+namespace SeeSharpHttpLiveStreaming.Tests.Playlist.Tags
+{
+    [TestFixture]
+    public class TestExtXDiscontinuity
+    {
+        private Discontinuity _discontinuity;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _discontinuity = new Discontinuity();
+            Assert.AreEqual("#EXT-X-DISCONTINUITY", _discontinuity.TagName);
+            Assert.AreEqual(TagType.ExtXDiscontinuity, _discontinuity.TagType);
+        }
+
+        [Test]
+        public void TestExtXDiscontinuityIsCreated()
+        {
+            Assert.IsNotNull(_discontinuity);
+        }
+
+        [Test]
+        public void TestExtXDiscontinuityDeserializeDoesNotThrowArgumeNullException()
+        {
+            _discontinuity.Deserialize(null, 0);
+        }
+
+        [Test]
+        public void TestExtXDiscontinuityDeserializeDoesNotThrowArgumeException()
+        {
+            _discontinuity.Deserialize(string.Empty, 0);
+        }
+
+        [Test]
+        public void TestExtXDiscontinuityThrowsSerializationExceptionIfAttributesProvided()
+        {
+            Assert.Throws<SerializationException>(() => _discontinuity.Deserialize("asasas", 0));
+        }
+
+    }
+
+    
+}
