623696822b424376d3a4ba83c027b9e6ee60221a
tpaananen <tommi.paananen@outlook.com>
2015-03-08 20:49:39 +0200

    EndList tag

---
+++ SeeSharpHttpLiveStreaming/Playlist/Tags/Media/EndList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SeeSharpHttpLiveStreaming.Playlist.Tags.Media
+{
+    /// <summary>
+    /// Represents the EXT-X-ENDLIST tag.
+    /// </summary>
+    /// <remarks>
+    /// The EXT-X-ENDLIST tag indicates that no more Media Segments will be
+    /// added to the Media Playlist file.  It MAY occur anywhere in the Media
+    /// Playlist file.  Its format is:
+    ///
+    /// #EXT-X-ENDLIST
+    /// </remarks>
+    public class EndList : BaseTag
+    {
+        /// <summary>
+        /// Gets the name of the tag, for example EXT-X-MEDIA.
+        /// </summary>
+        public override string TagName
+        {
+            get { return "#EXT-X-ENDLIST"; }
+        }
+
+        /// <summary>
+        /// Gets the type of the tag.
+        /// </summary>
+        public override TagType TagType
+        {
+            get { return TagType.ExtXEndList; }
+        }
+
+        /// <summary>
+        /// Deserializes the tag from the <paramref name="content"/>.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="version">The version.</param>
+        public override void Deserialize(string content, int version)
+        {
+            if (!string.IsNullOrEmpty(content))
+            {
+                throw new ArgumentException("The EXT-X-ENDLIST does not have attributes but attributes were provided.");
+            }
+        }
+    }
+}
---
+++ SeeSharpLiveStreaming.Tests/Playlist/Tags/Media/EndListTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using SeeSharpHttpLiveStreaming.Playlist;
+using SeeSharpHttpLiveStreaming.Playlist.Tags.Media;
+
+namespace SeeSharpHttpLiveStreaming.Tests.Playlist.Tags.Media
+{
+    [TestFixture]
+    public class EndListTests
+    {
+
+        private EndList _endList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _endList = new EndList();
+        }
+
+        [Test]
+        public void TestEndListIsCreated()
+        {
+            Assert.AreEqual("#EXT-X-ENDLIST", _endList.TagName);
+            Assert.AreEqual(TagType.ExtXEndList, _endList.TagType);
+        }
+
+        [Test]
+        public void TestEndListCanBeDeserializedWithNullContent()
+        {
+            _endList.Deserialize(null, 0);
+        }
+
+        [Test]
+        public void TestEndListCanBeDeserializedWithEmptyContent()
+        {
+            _endList.Deserialize(string.Empty, 0);
+        }
+
+        [Test]
+        public void TestEndListThrowsArgumentExceptionIfAttributesAreProvided()
+        {
+            Assert.Throws<ArgumentException>(() => _endList.Deserialize("1212", 0));
+        }
+    }
+}
