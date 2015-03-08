bb5513c83cc2785cf630e56f37928361d8ecbcdf
tpaananen <tommi.paananen@outlook.com>
2015-03-08 20:36:33 +0200

    Discontinuity sequence tag

---
+++ SeeSharpHttpLiveStreaming/Playlist/Tags/Media/DiscontinuitySequence.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using SeeSharpHttpLiveStreaming.Utils;
+using SeeSharpHttpLiveStreaming.Utils.ValueParsers;
+
+namespace SeeSharpHttpLiveStreaming.Playlist.Tags.Media
+{
+
+    /// <summary>
+    /// Represents the EXT-X-DISCONTINUITY-SEQUENCE tag.
+    /// </summary>
+    /// <remarks>
+    /// The EXT-X-DISCONTINUITY-SEQUENCE tag allows synchronization between
+    /// different Renditions of the same Variant Stream or different Variant
+    /// Streams that have EXT-X-DISCONTINUITY tags in their Media Playlists.
+    ///
+    /// Its format is:
+    ///
+    /// #EXT-X-DISCONTINUITY-SEQUENCE:&lt;number&gt;
+    /// 
+    /// If the Media Playlist does not contain an EXT-X-DISCONTINUITY-
+    /// SEQUENCE tag, then the Discontinuity Sequence Number of the first
+    /// Media Segment in the Playlist SHALL be considered to be 0.
+    ///
+    /// The EXT-X-DISCONTINUITY-SEQUENCE tag MUST appear before the first
+    /// Media Segment in the Playlist.
+    ///
+    /// The EXT-X-DISCONTINUITY-SEQUENCE tag MUST appear before any EXT-
+    /// X-DISCONTINUITY tag.
+    ///
+    /// See Section 6.2.1 and Section 6.2.2 for more information about
+    /// setting the value of the EXT-X-DISCONTINUITY-SEQUENCE tag.
+    /// </remarks>
+    public class DiscontinuitySequence : BaseTag
+    {
+        /// <summary>
+        /// Gets the name of the tag, for example EXT-X-MEDIA.
+        /// </summary>
+        public override string TagName
+        {
+            get { return "#EXT-X-DISCONTINUITY-SEQUENCE"; }
+        }
+
+        /// <summary>
+        /// Gets the type of the tag.
+        /// </summary>
+        public override TagType TagType
+        {
+            get { return TagType.ExtXDiscontinuitySequence; }
+        }
+
+        /// <summary>
+        /// Gets the Discontinuity Sequence Number.
+        /// </summary>
+        public long Number { get; private set; }
+
+        /// <summary>
+        /// Deserializes the tag from the <paramref name="content"/>.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="version">The version.</param>
+        public override void Deserialize(string content, int version)
+        {
+            content.RequireNotEmpty("content");
+            try
+            {
+                Number = ValueParser.ParseInt(content);
+            }
+            catch (Exception ex)
+            {
+                throw new SerializationException("Failed to parse " + TagName.Substring(1) + " tag.", ex);
+            }
+        }
+    }
+}
---
+++ SeeSharpLiveStreaming.Tests/Playlist/Tags/Media/DiscontinuitySequenceTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+using NUnit.Framework;
+using SeeSharpHttpLiveStreaming.Playlist;
+using SeeSharpHttpLiveStreaming.Playlist.Tags.Media;
+
+namespace SeeSharpHttpLiveStreaming.Tests.Playlist.Tags.Media
+{
+    [TestFixture]
+    public class DiscontinuitySequenceTests
+    {
+        private DiscontinuitySequence _discSeq;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _discSeq = new DiscontinuitySequence();
+            Assert.AreEqual("#EXT-X-DISCONTINUITY-SEQUENCE", _discSeq.TagName);
+            Assert.AreEqual(TagType.ExtXDiscontinuitySequence, _discSeq.TagType);
+        }
+
+        [Test]
+        public void TestParsingThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _discSeq.Deserialize(null, 0));
+        }
+
+        [Test]
+        public void TestParsingThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => _discSeq.Deserialize(string.Empty, 0));
+        }
+
+        [Test]
+        public void TestDeserializationFailes()
+        {
+            var exception = Assert.Throws<SerializationException>(() => _discSeq.Deserialize("sdsdsd", 0));
+            Assert.That(exception.InnerException is FormatException);
+        }
+
+        [Test]
+        public void TestDeserializationSucceeds()
+        {
+            _discSeq.Deserialize("4245", 0);
+            Assert.AreEqual(4245, _discSeq.Number);
+        }
+    }
+}
