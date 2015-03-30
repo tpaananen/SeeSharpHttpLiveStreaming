b88c2cfc115ebcd49ce06338d16707acbcbe8a6e
tpaananen <tommi.paananen@outlook.com>
2015-03-30 21:50:40 +0300

    Removed unused usings

--- SeeSharpHttpLiveStreaming/Utils/IncompatibleVersionException.cs
+++ SeeSharpHttpLiveStreaming/Utils/IncompatibleVersionException.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Runtime.Serialization;
 using System.Security.Permissions;
-using SeeSharpHttpLiveStreaming.Playlist.Tags;
 
 namespace SeeSharpHttpLiveStreaming.Utils
 {
--- SeeSharpLiveStreaming.Tests/Playlist/Tags/Media/MediaSegment/KeyTests.cs
+++ SeeSharpLiveStreaming.Tests/Playlist/Tags/Media/MediaSegment/KeyTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Runtime.Serialization;
 using System.Text;
 using NUnit.Framework;
